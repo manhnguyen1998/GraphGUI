8f547c059f31203c2a3c95c316ff137f3369bf38
thanhcong2298 <thanhcong.phung@gmail.com>
2019-05-05 10:42:29 +0700

    Update Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         primaryStage = stage;
-        Parent root = FXMLLoader.load(getClass().getResource("ex.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
